62b54bef51bd7e4903103057b48d346f83d9b6d7
Anurag <anuragyadav41682@gmail.com>
2023-10-04 16:20:41 +0530

    UPDATED API

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-const url = "https://memories-web-app-pi.vercel.app/";
+const url = "https://memories-web-app-pi.vercel.app";
 // const url = "http://localhost:3001/posts";
 const API = axios.create({ baseURL: url });
 API.interceptors.request.use((req) => {
@@ -13,9 +13,9 @@ API.interceptors.request.use((req) => {
 export const fetchPosts = () => API.get("/posts");
 export const createPost = (newPost) => API.post("/posts", newPost);
 export const updatePost = (id, updatedPost) =>
-  API.patch(`posts/${id}`, updatedPost);
-export const deletePost = (id) => API.delete(`posts/${id}`);
-export const likePost = (id) => API.patch(`posts/${id}/likePost`);
+  API.patch(`/posts/${id}`, updatedPost);
+export const deletePost = (id) => API.delete(`/posts/${id}`);
+export const likePost = (id) => API.patch(`/posts/${id}/likePost`);
 
 export const signIn = (formData) => API.post("/users/signin", formData);
 export const signUp = (formData) => API.post("/users/signup", formData);
